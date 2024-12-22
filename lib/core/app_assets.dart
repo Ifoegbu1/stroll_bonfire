33f03b07724ab2fc79e43d81d65d6e06b5aa2907
Charles Ifoegbu <charlesifoegbu@gmail.com>
2024-12-22 15:42:27 +0100

    added bottom bar assets and assets_generator

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,14 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class AppAssets {
+  const AppAssets._();
+
+  static const String ASSETS_ICONS_BONFIRE_SVG = 'assets/icons/bonfire.svg';
+
+  static const String ASSETS_ICONS_CARD_SVG = 'assets/icons/card.svg';
+
+  static const String ASSETS_ICONS_CHAT_SVG = 'assets/icons/chat.svg';
+
+  static const String ASSETS_ICONS_USER_SVG = 'assets/icons/user.svg';
+}
